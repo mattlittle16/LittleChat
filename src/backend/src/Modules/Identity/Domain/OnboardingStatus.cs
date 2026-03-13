51849e5556f2bdea0674f53272fa969dd3658911
Matt Little <mituw16@gmail.com>
2026-03-13 16:12:15 -0400

    [master] wizard fixes

--- src/backend/src/Modules/Identity/Domain/OnboardingStatus.cs
+++ src/backend/src/Modules/Identity/Domain/OnboardingStatus.cs
@@ -1,11 +1,8 @@
-using System.Text.Json.Serialization;
-
 namespace Identity.Domain;
 
-[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum OnboardingStatus
 {
-    [JsonPropertyName("not_started")] NotStarted,
-    [JsonPropertyName("remind_later")] RemindLater,
-    [JsonPropertyName("dismissed")]    Dismissed,
+    NotStarted,
+    RemindLater,
+    Dismissed,
 }
